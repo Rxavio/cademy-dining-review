7db5322eb44df4c13e247af0f893aeeb1ff16b52
xavioo <rucahoxavi@gmail.com>
2021-11-29 11:25:52 +0200

    create project and configure postgres db

---
+++ cademy-dining-review/src/main/java/net/dining/springboot/CademyDiningReviewApplication.java
@@ -0,0 +1,13 @@
+package net.dining.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CademyDiningReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CademyDiningReviewApplication.class, args);
+	}
+
+}
---
+++ cademy-dining-review/src/test/java/net/dining/springboot/CademyDiningReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dining.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CademyDiningReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
