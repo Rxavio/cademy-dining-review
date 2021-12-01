6fd5918d0dc6d34556a5e5ef5d7cb0afb0dd65f1
xavioo <rucahoxavi@gmail.com>
2021-12-01 13:28:19 +0200

    approve review status

--- cademy-dining-review/src/main/java/net/dining/springboot/controller/DiningReviewController.java
+++ cademy-dining-review/src/main/java/net/dining/springboot/controller/DiningReviewController.java
@@ -1,10 +1,11 @@
 package net.dining.springboot.controller;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -70,6 +71,17 @@ public class DiningReviewController {
     @GetMapping("/dining-review/rejected/{id}")
     public Iterable<DiningReview> getRejectedReviewById(@PathVariable("id") Long id){
         return reviewRepository.findByIdAndAdminReviewStatus(id, AdminReviewStatus.REJECTED);
+    }
+    
+    @PutMapping("/dining-review/{id}/approve")
+    public DiningReview approveReview(@PathVariable("id") Long id){
+        Optional<DiningReview> reviewOptional = reviewRepository.findById(id);
+        if(reviewOptional.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This review does not exist");
+        }
+        DiningReview reviewToApprove = reviewOptional.get();
+        reviewToApprove.setAdminReviewStatus(AdminReviewStatus.APPROVED);
+        return reviewRepository.save(reviewToApprove);
     }
 
   
