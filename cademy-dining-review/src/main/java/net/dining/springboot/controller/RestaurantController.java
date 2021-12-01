9b108469bbd7e2e1bc80acda5cf8b9112eb2cf2d
xavioo <rucahoxavi@gmail.com>
2021-12-01 09:48:07 +0200

    find all restaurant

--- cademy-dining-review/src/main/java/net/dining/springboot/controller/RestaurantController.java
+++ cademy-dining-review/src/main/java/net/dining/springboot/controller/RestaurantController.java
@@ -2,6 +2,7 @@ package net.dining.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,10 @@ public class RestaurantController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A restaurant with this name and postcode already exists");
         }
         return restaurantRepository.save(restaurant);
+    }
+    
+    @GetMapping("/restaurants")
+    public Iterable<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
     }
 }
