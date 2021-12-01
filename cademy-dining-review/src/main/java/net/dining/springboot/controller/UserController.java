475c4eab0afc9a8c8ce7c91f8b92a4c3a8221054
xavioo <rucahoxavi@gmail.com>
2021-12-01 04:32:45 +0200

    delete user

--- cademy-dining-review/src/main/java/net/dining/springboot/controller/UserController.java
+++ cademy-dining-review/src/main/java/net/dining/springboot/controller/UserController.java
@@ -1,23 +1,53 @@
 package net.dining.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.dining.springboot.model.User;
 import net.dining.springboot.repository.UserRepository;
 
+
 @RestController
+//@RequestMapping("api/v1")
 public class UserController {
-    @Autowired
-    UserRepository userRepository;
+//    @Autowired
+//    UserRepository userRepository;
+	
+	private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/users/adduser")
     public User createNewUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
     
+    @GetMapping("/users")
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
 
+    }
+    
+    @DeleteMapping("/users/{id}")
+    public User deleteUser(@PathVariable("id") Long id) {
+        Optional<User> optionalUser = this.userRepository.findById(id);
+
+        if(optionalUser.isEmpty()) {
+            return null;
+        }
+        User userToDelete = optionalUser.get();
+        this.userRepository.delete(userToDelete);
+        return userToDelete;
+    }
 
   }
--- cademy-dining-review/src/main/java/net/dining/springboot/model/User.java
+++ cademy-dining-review/src/main/java/net/dining/springboot/model/User.java
@@ -1,20 +1,13 @@
 package net.dining.springboot.model;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     
     @Column(name = "username")
     private String userName;
@@ -38,11 +31,11 @@ public class User {
     private boolean isDairyAllergy;
     
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
